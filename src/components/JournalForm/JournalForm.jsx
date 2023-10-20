c0f38cba22a8e9d4d2666ea3af1901a28d50ca29
Gosudarev Danila <danidagosudarev@gmail.com>
2023-10-20 21:12:22 +0300

    35-40

--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -1,43 +1,21 @@
-import { useState } from 'react';
 import './JournalForm.css';
 import Button from '../Button/Button';
 
-function JournalForm() {
-	const [inputData, setInputData] = useState('');
-	const [state, setState] = useState({
-		age: 31
-	});
-
-	const [state2, setState2] = useState([
-		1,2,3
-	]);
-
-	const inputChange = (event) => {
-		setInputData(event.target.value);
-		console.log(inputData);
-	};
-
+function JournalForm({onSubmit}) {
 	const addJournalIten = (e) => {
 		e.preventDefault();
-		state.age = 40;
-		console.log(state);
-		// state2.push(4);
-		// setState2([...state2]);
-		setState({...state});
 		const formData = new FormData(e.target);
 		const formProps = Object.fromEntries(formData);
-		console.log(formProps);
+		onSubmit(formProps);
 	};
 
 	return (
 		<>
 			<form className='journal-form' onSubmit={addJournalIten}>
-				{state.age}
-				{state2.length}
 				<input type="text" name="title"/>
 				<input type="date" name="date"/>
-				<input type="text" name="tag" value={inputData} onChange={inputChange}/>
-				<textarea name="post" id="" cols="30" rows="10"></textarea>
+				<input type="text" name="tag"/>
+				<textarea name="text" id="" cols="30" rows="10"></textarea>
 				<Button text="Сохранить" onClick={() => console.log('Нажали')}/>
 			</form>
 		</>
