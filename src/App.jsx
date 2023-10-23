87ecb08a71d47029e5ec57f1f714bc6af923bc1f
Gosudarev Danila <danidagosudarev@gmail.com>
2023-10-23 20:19:50 +0300

    43-51

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,26 @@ import JournalList from './components/JournalList/JournalList';
 import Header from './components/Header/Header';
 import JournalAddButton from './components/JournalAddButton/JournalAddButton';
 import JournalForm from './components/JournalForm/JournalForm';
-import { useState } from 'react';
-
-const INITIAL_DATA = [
-	// {
-	// 	id: 1,
-	// 	title: 'Подготовка к обновлению курсов',
-	// 	text:'Горные походы открывают удивительные природные ландшафт',
-	// 	date: new Date()
-	// },
-	// {
-	// 	id: 2,
-	// 	title: 'Поход в горы',
-	// 	text:'Думал, что очень много времени',
-	// 	date: new Date()
-	// }
-];
+import { useEffect, useState } from 'react';
 
 function App() {
-	const [items, setItems] = useState(INITIAL_DATA);
+	const [items, setItems] = useState([]);
+
+	useEffect(() => {
+		const data = JSON.parse(localStorage.getItem('data'));
+		if (data) {
+			setItems(data.map(item => ({
+				...item,
+				date: new Date(item.date)
+			})));
+		}
+	}, []);
+
+	useEffect(() => {
+		if (items.length) {
+			localStorage.setItem('data', JSON.stringify(items));
+		}
+	}, [items]);
 
 	const addItem = item => {
 		setItems(oldItems => [...oldItems, {
--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -1,22 +1,68 @@
-import './JournalForm.css';
+import styles from './JournalForm.module.css';
 import Button from '../Button/Button';
+import { useEffect, useReducer} from 'react';
+import cn from 'classnames';
+import { INITIAL_STATE, formReducer } from './JournalForm.state';
 
 function JournalForm({onSubmit}) {
+	const [formState, dispatchForm] = useReducer(formReducer, INITIAL_STATE);
+	const { isValid, isFormReadyToSubmit, values } = formState;
+
+	useEffect(() => {
+		let timerId;
+		if (!isValid.date || !isValid.text || !isValid.title) {
+			timerId = setTimeout(() => {
+				dispatchForm({type: 'RESET_VALIDITY'});
+			}, 2000);
+		}
+		return () => {
+			clearTimeout(timerId);
+		};
+	}, [isValid]);
+
+	useEffect(() => {
+		if (isFormReadyToSubmit) {
+			onSubmit(values);
+		}
+	}, [isFormReadyToSubmit]);
+
 	const addJournalIten = (e) => {
 		e.preventDefault();
 		const formData = new FormData(e.target);
 		const formProps = Object.fromEntries(formData);
-		onSubmit(formProps);
+
+		dispatchForm({ type: 'SUBMIT', payload: formProps });
 	};
 
 	return (
 		<>
-			<form className='journal-form' onSubmit={addJournalIten}>
-				<input type="text" name="title"/>
-				<input type="date" name="date"/>
-				<input type="text" name="tag"/>
-				<textarea name="text" id="" cols="30" rows="10"></textarea>
-				<Button text="Сохранить" onClick={() => console.log('Нажали')}/>
+			<form className={styles['journal-form']} onSubmit={addJournalIten}>
+				<div>
+					<input type="text" name="title" className={cn(styles['input-title'], {
+						[styles['invalid']]: !isValid.title
+					})}/>
+				</div>
+				<div className={styles['from-row']}>
+					<label htmlFor="date" className={styles['form-label']}>
+						<img src="/calendar.svg" alt="Иконка календаря" />
+						<span>Дата</span>
+					</label>
+					<input type="date" name="date" id='date' className={cn(styles['input'], {
+						[styles['invalid']]: !isValid.date
+					})}/>
+				</div>
+				<div className={styles['from-row']}>
+					<label htmlFor="tag" className={styles['form-label']}>
+						<img src="/folder.svg" alt="Иконка папки" />
+						<span>Метки</span>
+					</label>
+					<input type="text" id='tag' name="tag" className={styles['input']} />
+				</div>
+				
+				<textarea name="text" id="" cols="30" rows="10" className={cn(styles['input'], {
+					[styles['invalid']]: !isValid.text
+				})}></textarea>
+				<Button text="Сохранить" />
 			</form>
 		</>
 	);
